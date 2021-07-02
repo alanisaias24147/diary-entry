3cc68349705223ad55d7321ad02dc06b6c19c335
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-07-02 07:56:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Diary_Entry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter palabras;
+            palabras = new StreamWriter("texto.txt");
+            Console.WriteLine("ingresa un texto");
+            string mensaje = Console.ReadLine();
+
+            DateTime fechaDehoy = DateTime.Now;
+            CultureInfo culture = new CultureInfo("es-MX");
+            string fechaDehoyStr = fechaDehoy.ToString(culture);
+
+            Console.WriteLine(fechaDehoyStr);
+            palabras.WriteLine(mensaje);
+
+        }
+    }
+}
